3789fffe5450e8022b12274da84ed8052b74e241
antlaw0 <antlaw0@gmail.com>
2022-10-20 14:31:57 -0500

    Made real_database var

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 import os
 
-uri = os.getenv("DATABASE_URL")  # or other relevant config var
+uri = os.getenv("REAL_DATABASE_URL")  # or other relevant config var
 #if uri.startswith("postgres://"):
     #uri = uri.replace("postgres://", "postgresql://", 1)
 # rest of connection code using the connection string `uri`
